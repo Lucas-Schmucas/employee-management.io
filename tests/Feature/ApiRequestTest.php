09a9cb71c4a131622a5bf2eeb3807ab15d30306d
Lucas <lucas.breiner@gmx.de>
2023-09-22 23:59:54 +0200

    implement test for api post request

---
+++ tests/Feature/ApiRequestTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class ApiRequestTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test_employee_post_api()
+    {
+        // "curl -X POST -H 'Content-Type: text/csv' -d @import.csv http://employee-management.io/api/employee";
+
+        $file = file_get_contents('./tests/import.csv');
+
+        $response = $this->postJson('/api/employee', [$file], [
+            'Content-Type' => 'text/csv',
+        ]);
+
+        $response->assertStatus(201);
+
+        $response->assertJson([
+            'created' => true,
+        ]);
+        $this->assertDatabaseCount('employees', 10001);
+
+        $this->assertDatabaseHas('employees', [
+            'email' => 'avelina.stoner@exxonmobil.com' // random pick
+        ]);
+
+    }
+}
