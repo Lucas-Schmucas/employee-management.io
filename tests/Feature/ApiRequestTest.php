6295abfb49415ab6dc4b868f51e806d96837f0a7
Lucas <lucas.breiner@gmx.de>
2023-09-23 20:21:36 +0200

    refine tests for post request

--- tests/Feature/ApiRequestTest.php
+++ tests/Feature/ApiRequestTest.php
@@ -10,22 +10,20 @@ class ApiRequestTest extends TestCase
 {
     use DatabaseTransactions;
 
-    public function test_employee_post_api()
+    public function test_employee_create()
     {
-        // "curl -X POST -H 'Content-Type: text/csv' -d @import.csv http://employee-management.io/api/employee";
+        // "curl -X POST -H 'Content-Type: text/csv' -d @import.csv http://172.0.10.1:7777/api/employee";
 
-        $file = file_get_contents('./tests/import.csv');
+        $file = file_get_contents('./tests/smallImport.csv');
 
-        $response = $this->postJson('/api/employees', [$file], [
-            'Content-Type' => 'text/csv',
-        ]);
+        $response = $this->post('/api/employee', [$file], ['Content-Type' => 'text/csv']);
 
         $response->assertStatus(201);
 
         $response->assertJson([
             'created' => true,
         ]);
-        $this->assertDatabaseCount('employees', 10001);
+        $this->assertDatabaseCount('employees', 10000);
 
         $this->assertDatabaseHas('employees', [
             'email' => 'avelina.stoner@exxonmobil.com' // random pick
