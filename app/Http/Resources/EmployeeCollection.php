b37b2b345d6e1c6159abdf3cd00b50107d80e52f
Lucas <lucas.breiner@gmx.de>
2023-09-24 14:53:13 +0200

    implement flag for user response and testing

--- app/Http/Resources/EmployeeCollection.php
+++ app/Http/Resources/EmployeeCollection.php
@@ -14,6 +14,9 @@ class EmployeeCollection extends ResourceCollection
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'created' => true
+            ];
     }
 }
