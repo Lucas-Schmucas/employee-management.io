5ff9ec5010933237d53d087e711fb1be15bb6268
Lucas <lucas.breiner@gmx.de>
2023-09-25 00:17:29 +0200

    implement mutation for date and time

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
@@ -9,6 +11,14 @@ use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'date_of_birth' => 'datetime:Y-m-d',
+    ];
+    protected $appends = [
+        'age_in_years',
+        'age_in_company'
+    ];
 
     protected $fillable = [
         'id',
@@ -25,8 +35,32 @@ class Employee extends Model
         'phone_number',
     ];
 
-    public function address()  : HasOneOrMany
+    public function address(): HasOneOrMany
     {
         return $this->hasMany(Address::class);
+    }
+
+    protected function timeOfBirth(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => Carbon::createFromFormat('H:i:s', $value)->format('h:i:s A'),
+            set: fn(string $value) => Carbon::createFromFormat('h:i:s A', $value)->format('H:i:s'),
+        );
+    }
+
+    protected function dateOfBirth(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => Carbon::createFromFormat('Y-m-d', $value)->format('m/d/Y'),
+            set: fn(string $value) => Carbon::createFromFormat('m/d/Y', $value)->format('Y-m-d'),
+        );
+    }
+
+    protected function dateOfJoining(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => Carbon::createFromFormat('Y-m-d', $value)->format('m/d/Y'),
+            set: fn(string $value) => Carbon::createFromFormat('m/d/Y', $value)->format('Y-m-d'),
+        );
     }
 }
