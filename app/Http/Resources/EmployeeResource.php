55b0ac5f8c3d849c84b13f0177ed85779fa6af3c
Lucas <lucas.breiner@gmx.de>
2023-09-26 00:26:41 +0200

    extend employee resource with mapping

--- app/Http/Resources/EmployeeResource.php
+++ app/Http/Resources/EmployeeResource.php
@@ -14,6 +14,26 @@ class EmployeeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'Emp ID' => $this->id,
+            'User Name' => $this->username,
+            'Name Prefix' => $this->prefix,
+            'First Name' => $this->firstname,
+            'Middle Initial' => $this->middle_initial,
+            'Last Name' => $this->lastname,
+            'Gender' => $this->gender,
+            'E Mail' => $this->email,
+            'Date of Birth' => $this->data_of_birth,
+            'Time of Birth' => $this->time_of_birth,
+            'Age in Yrs.' => $this->age_in_years,
+            'Date of Joining' => $this->date_of_joining,
+            'Age in Company (Years)' => $this->age_in_company,
+            'Phone No. ' => $this->phone_number,
+            'Country' => $this->address->country,
+            'Place Name' => $this->address->street,
+            'City' => $this->address->street,
+            'Zip' => $this->address->zip,
+            'Region' => $this->address->region,
+        ];
     }
 }
